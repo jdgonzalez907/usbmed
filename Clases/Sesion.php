03640b1c8f19be0ef76735cdebe8575471b59eee
ingeniero.analista1 <ingeniero.analista1@equipo938.USB.LOCAL>
2017-03-09 14:46:32 -0500

    Estructura

---
+++ Clases/Sesion.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Clases\Sesion;
+
+/**
+ * Description of Sesion
+ *
+ * @author ingeniero.analista1
+ */
+class Sesion {
+    //put your code here
+}
