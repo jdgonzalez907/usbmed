71049b7e0e2fef40bf661d3b3f5d6ef08e0868d9
ingeniero.analista1 <ingeniero.analista1@equipo938.USB.LOCAL>
2017-03-10 10:03:32 -0500

    Estructura

--- application/View/home/index.php
+++
@@ -1,6 +0,0 @@
-
-    <h1>Home</h1>
-    <h2>You are in the View: application/view/home/index.php (everything in the box comes from this file)</h2>
-    <p>In a real application this could be the homepage.</p>
-    
-    
