575014901b6c463a52eb567b69beabb26460efbb
ingeniero.analista1 <ingeniero.analista1@equipo938.USB.LOCAL>
2017-04-05 10:13:08 -0500

    Representante

--- application/View/_templates/menu_top/footer.php
+++ application/View/_templates/menu_top/footer.php
@@ -3,6 +3,21 @@
             </div>
         </div>
         
+        <footer class="small">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-sm-offset-3 text-center">
+                        <p>© Aplicaciones USB Medellín <?= date('Y') ?></p>
+                        <p>
+                            Detalles de contácto:
+                            <span class="glyphicon glyphicon-earphone"></span>  57(4) 514 5600 ext 4337 - 
+                            <a href="mailto:ingeniero.analista1@usbmed.edu.co"><span class="glyphicon glyphicon-envelope"></span> ingeniero.analista1@usbmed.edu.co</a>
+                        </p>
+                    </div>
+                </div>
+            </div>  
+        </footer>
+        
         <!-- Modal -->
         <div class="modal fade" id="modal_usbmed" tabindex="-1" role="dialog" aria-labelledby="modal_usbmed_title">
             <div class="modal-dialog" id="modal_usbmed_tipo_modal" role="document">
