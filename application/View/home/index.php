07f3c12b4e726caccf87c8fceb51cb2ab86d5353
ingeniero.analista1 <ingeniero.analista1@equipo938.USB.LOCAL>
2017-04-05 12:31:18 -0500

    Representante

--- application/View/home/index.php
+++ application/View/home/index.php
@@ -22,29 +22,43 @@
             <ol class="carousel-indicators">
                 <li data-target="#carousel-usbmed" data-slide-to="0" class="active"></li>
                 <li data-target="#carousel-usbmed" data-slide-to="1"></li>
+                <li data-target="#carousel-usbmed" data-slide-to="2"></li>
+                <li data-target="#carousel-usbmed" data-slide-to="3"></li>
             </ol>
 
             <!-- Wrapper for slides -->
             <div class="carousel-inner" role="listbox">
                 <div class="item active">
-                    <img src="<?= URL ?>img/carousel/campus1.jpg" class="img-responsive center-block" alt="Campus">
+                    <img src="<?= URL ?>img/carousel/campus1.jpg" class="img-responsive img-thumbnail img-carousel center-block" alt="Campus">
                     <div class="carousel-caption">
 
                     </div>
                 </div>
                 <div class="item">
-                    <img src="<?= URL ?>img/carousel/san_benito1.jpg" class="img-responsive center-block" alt="San Benito">
+                    <img src="<?= URL ?>img/carousel/san_benito1.jpg" class="img-responsive img-thumbnail img-carousel center-block" alt="San Benito">
+                    <div class="carousel-caption">
+
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="<?= URL ?>img/carousel/armenia1.jpg" class="img-responsive img-thumbnail img-carousel center-block" alt="Armenia">
+                    <div class="carousel-caption">
+
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="<?= URL ?>img/carousel/ibague1.jpg" class="img-responsive img-thumbnail img-carousel center-block" alt="Ibague">
                     <div class="carousel-caption">
 
                     </div>
                 </div>
             </div>
 
-            
+
         </div>
     </div>
     <div class="col-sm-4">
-        <legend>Sus aplicativos:</legend>
+        <legend class="text-primary">Sus aplicativos:</legend>
         <p>A continuación se listarán los aplicativos a los que tiene acceso:</p>
         <div class="list-group">
             <?php
@@ -55,5 +69,28 @@
             }
             ?>
         </div>
+    </div>
+</div>
+<div class="row presentacion">
+    <div class="col-sm-4">
+        <h3 class="text-center">Seguridad</h3>
+        <img class="img-responsive center-block img-presentacion" src="<?= URL ?>img/computer.png" alt="">        
+        <p class="text-justify">
+            La protección de datos es algo sumamente importante, por lo cuál se optó por tecnologías que brinden la seguridad necesaria para mantener la inforamción segura.
+        </p>
+    </div>
+    <div class="col-sm-4">
+        <h3 class="text-center">Adaptación</h3>
+        <img class="img-responsive center-block img-presentacion" src="<?= URL ?>img/smartphone.png" alt="">        
+        <p class="text-justify">
+            Estamos en un mundo donde los smartphone's se usan cada vez más... teniendo en cuenta esto, se diseñó un sistema que se acomode a la pantalla de cualquier dispositivo.
+        </p>
+    </div>
+    <div class="col-sm-4">
+        <h3 class="text-center">Sencillez</h3>
+        <img class="img-responsive center-block img-presentacion" src="<?= URL ?>img/browser.png" alt="">        
+        <p class="text-justify">
+            A nosotros también nos gustan las cosas que nos faciliten la vida, por ello el diseño se basa en un sistema práctico, seguro y amigable.
+        </p>
     </div>
 </div>
