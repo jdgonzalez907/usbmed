2240d2cb5cd3858491081de1793ed29f9e4f4de2
ingeniero.analista1 <ingeniero.analista1@equipo938.USB.LOCAL>
2017-04-22 12:19:13 -0500

    DerechoGrados

--- application/View/_templates/derechoGrados/footer.php
+++ application/View/_templates/derechoGrados/footer.php
@@ -3,20 +3,67 @@
             </div>
         </div>
         
-        <footer class="small">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-sm-offset-3 text-center">
-                        <p>© Aplicaciones USB Medellín <?= date('Y') ?></p>
-                        <p>
-                            Detalles de contácto:
-                            <span class="glyphicon glyphicon-earphone"></span>  57(4) 514 5600 ext 4337 - 
-                            <a href="mailto:ingeniero.analista1@usbmed.edu.co"><span class="glyphicon glyphicon-envelope"></span> ingeniero.analista1@usbmed.edu.co</a>
-                        </p>
+        <!--Inicio Footer -->
+        <style>
+            div.PieGral {border-top:solid; border-top-width:10px; border-top-color:#F4811E; background-color:#333; border-bottom:solid; border-bottom-width:1px; border-bottom-color:#BBB; }
+            div.Direcciones {background-color:#000; padding:10px; }
+            div.Legales { background-color:#FFF; padding:5px; }
+            .TxtPie1 { font-family: 'Open Sans', sans-serif; font-size:15px; font-weight:700; color:#FFF; line-height:25px; }
+            .TxtPie2 { font-family: 'Open Sans', sans-serif; font-size:15px; font-weight:400; color:#FFF; line-height:25px;}
+            .TxtLegales { font-family: 'Open Sans', sans-serif; font-size:12px; font-weight:400; color:#000; line-height:15px; 
+            }
+ 
+        </style>
+        <div class="SeparaPie"></div>
+        <div class="PieGral"><div id="dnn_FooterPane"><div class="DnnModule DnnModule-DNN_HTML DnnModule-411"><a name="411"></a>
+ 
+                    <div class="C_contenedor">
+                        <div class="C_contenido" id="dnn_ctr411_CONTENTPANE"><!-- Start_Module_411 --><div class="DNNModuleContent ModDNNHTMLC" id="dnn_ctr411_ModuleContent">
+                                <div class="Normal" id="dnn_ctr411_HtmlModule_lblContent">
+                                    <div class="principal" style="padding-top: 30px;">
+                                        <div class="rwd_col1">
+                                            <div class="margen10">
+ 
+                                                <div class="principal">
+                                                  </div><!-- End_Module_411 --></div>
+                                        </div>
+ 
+                                    </div></div></div>
+                            <div class="Direcciones"><div id="dnn_AddressesPane"><div class="DnnModule DnnModule-DNN_HTML DnnModule-412"><a name="412"></a>
+ 
+                                        <div class="C_contenedor">
+                                            <div class="C_contenido" id="dnn_ctr412_CONTENTPANE"><!-- Start_Module_412 --><div class="DNNModuleContent ModDNNHTMLC" id="dnn_ctr412_ModuleContent">
+                                                    <div class="Normal" id="dnn_ctr412_HtmlModule_lblContent">
+                                                        <div class="principal">
+                                                            <div class="rwd_col1">
+ 
+                                                            </div>
+ 
+                                                            <div class="rwd_col3 Centrar text-center"><span class="TxtPie2">• Medellín: San Benito: Carrera 56C N° 51-110 Centro • Bello: Calle 45 N° 61-40 Barrio Salento<br>
+                                                                    • Armenia: Carrera 13 N° 1ª Norte- 50 • Ibagué: Carrera 5N°24-12 Barrio Hipódromo</span></div>
+ 
+                                                            <div class="reset">&#160;</div>
+                                                        </div>
+ 
+                                                    </div>
+ 
+                                                </div><!-- End_Module_412 --></div>
+                                        </div>
+ 
+                                    </div></div></div>
+                            <div class="Legales text-center"><div id="dnn_LegalPane"><div class="style1">
+                                        <span class="style2">© 2017 WWW.USBMED.EDU.CO • 
+                                            <a href="https://usbmedellin.sharepoint.com/sites/intranet/política-de-privacidad" target="_blank">POLÍTICA DE PRIVACIDAD</a> • <a href="#">TÉRMINOS Y CONDICIONES</a>
+ 
+                                            La Universidad de San Buenaventura Medellín es una Institución de Educación Superior sujeta a inspección y vigilancia por el Ministerio de Educación Nacional</span><a name="413"></a><!-- End Search --> 
+                                        <!-- End Footer --><br/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>  
-        </footer>
+                    <!--end workspace-->
+
 
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
